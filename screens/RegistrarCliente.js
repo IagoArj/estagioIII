497b15eae80787fdad95887eafecd8ac2f7abf9e
iagoarj <emerson_iago@hotmail.com>
2019-10-24 21:52:51 -0300

    atualizando

--- screens/RegistrarCliente.js
+++ screens/RegistrarCliente.js
@@ -3,93 +3,95 @@ import firebase from 'firebase';
 import { View, TextInput, TouchableOpacity, Text, ScrollView, StyleSheet } from 'react-native';
 
 const RegistrarCliente = props => {
-    state={
-        nomeState:nome,
-        telefoneState:telefone,
-        limiteContaState:limiteConta,
-        permissaoState:permissao
+    state = {
+        nomeState: nome,
+        telefoneState: telefone,
+        limiteContaState: limiteConta,
+        permissaoState: permissao
     }
-    
+
     var [nome, onChangeNome] = React.useState('Nome');
     var [telefone, onChangeTelefone] = React.useState("Telefone");
     var [limiteConta, onChangeLimiteConta] = React.useState("Limite Da Conta");
     var [permissao, onChangePermissao] = React.useState('Permissão');
     var [endereco, onChangeEndereco] = React.useState("Endereço")
-    
-return(
-    <View>
-        <TextInput style={styles.input}
-        onChangeText={text => onChangeNome(text)}
-        value={nome}
-        />
-        <TextInput style={styles.input}
-        onChangeText={text => onChangeTelefone(text)}
-        keyboardType ='numeric'
-        value={telefone}
-        />
-        <TextInput style={styles.input}
-        onChangeText={text => onChangeLimiteConta(text)}
-        value={limiteConta}
-        keyboardType ='numeric'
-        />
-        <TextInput style={styles.input}
-        onChangeText={text => onChangePermissao(text)}
-        value={permissao}
-        />
-        <TextInput style={styles.input}
-        onChangeText={text => onChangeEndereco(text)}
-        value={endereco}
-        />
-        <TouchableOpacity style={styles.btn} onPress={() => {
-           var date = new Date().getDate(); //Current Date
-           var month = new Date().getMonth() + 1; //Current Month
-           var year = new Date().getFullYear(); //Current Year
-           var hours = new Date().getHours(); //Current Hours
-           var min = new Date().getMinutes(); //Current Minutes
-           var seconds = new Date().getSeconds();
 
-           const diaConta = date + '/' + month + '/' + '/' + year + ' ' + hours + ':' + min + ':' + seconds
+    return (
+        <View>
+            <TextInput style={styles.input}
+                onChangeText={text => onChangeNome(text)}
+                value={nome}
+            />
+            <TextInput style={styles.input}
+                onChangeText={text => onChangeTelefone(text)}
+                keyboardType='numeric'
+                value={telefone}
+            />
+            <TextInput style={styles.input}
+                onChangeText={text => onChangeLimiteConta(text)}
+                value={limiteConta}
+                keyboardType='numeric'
+            />
+            <TextInput style={styles.input}
+                onChangeText={text => onChangePermissao(text)}
+                value={permissao}
+            />
+            <TextInput style={styles.input}
+                onChangeText={text => onChangeEndereco(text)}
+                value={endereco}
+            />
+            <TouchableOpacity style={styles.btn} onPress={() => {
+                var data = new Date().getDate(); //Current Date
+                var month = new Date().getMonth() + 1; //Current Month
+                var year = new Date().getFullYear(); //Current Year
+                var hours = new Date().getHours(); //Current Hours
+                var min = new Date().getMinutes(); //Current Minutes
+                var seconds = new Date().getSeconds();
+                var idUsuario = Math.random().toString(36).substr(2, 9);
+                const cliente = {
+                    id: idUsuario,
+                    nome: nome,
+                    telefone: telefone,
+                    dataConta: data + '/' + month + '/' + year + ' ' + hours + ':' + min + ':' + seconds,
+                    endereco: endereco,
+                    autCompra: [permissao],
+                    ultimaCompra: '',
+                    conta: {
+                        limiteConta: limiteConta,
+                        totalPagar: 0,
+                        saldo: limiteConta,
+                        compras: []
+                    }
+                }
 
-        firebase.database().ref('clientes/'+ 3).set(
-            {
-               
-                id: Math.random().toString(36).substr(2, 9),
-                nome: nome,
-                telefone: telefone,
-                dataConta: diaConta,
-                endereco: endereco,
-                autCompra: [],
-                ultimaCompra: '',
-                conta: {
-                    limiteConta: limiteConta,
-                    totalPagar: 0,
-                    saldo: limiteConta,
-                    compras:[]
-                },
-            
+                firebase.database().ref('clientes/' + idUsuario).set(
+                    {
+                        cliente
+                    }
+                ).then(() => {
+                    console.log('enviado');
+                }).catch((error) => {
+                    console.log(error)
+                })
+                props.navigation.navigate('ClientPage', {
+                    cliente
+                })
             }
-        ).then(()=>{
-            console.log('enviado');
-        }).catch((error)=>{
-            console.log(error)
-        })
-
-        }
-      } >
-        <Text style={{ color: 'white' ,  fontWeight:"bold"}}> criar </Text>
-      </TouchableOpacity>
-    </View>
-)
+            } >
+                <Text style={{ color: 'white', fontWeight: "bold" }}> criar </Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
-RegistrarCliente.navigationOptions={
-    title:'Registrar Cliente',
+RegistrarCliente.navigationOptions = {
+    title: 'Registrar Cliente',
     headerTintColor: '#fff',
-    headerStyle:{
-        backgroundColor:'#641e82',
+    headerStyle: {
+        backgroundColor: '#641e82',
     }
-  };
-  const styles = StyleSheet.create({
-    input:{
+};
+const styles = StyleSheet.create({
+    input: {
         backgroundColor: '#531a6b',
         width: 300,
         marginTop: 15,
@@ -106,7 +108,7 @@ RegistrarCliente.navigationOptions={
         padding: 10,
         marginTop: 20,
         borderRadius: 30
-      },
+    },
 
 
 })
